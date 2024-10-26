c3ed7010473714f42b1f65e5a97ff5c8246e2fb8
Shahzada Modassir <152081666+shahzadamodassir@users.noreply.github.com>
2024-10-27 00:20:05 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en" data-build-timestamp="14-10-2023 at 12:36 (UTC+12:40)" data-version="14.10.23">
+<head>
+  <title>Notes Manager</title>
+  <meta name="application-name" content="Notes Manager">
+  <meta itemprop="name" content="Notes Manager">
+  <meta name="author" content="Expert Modassir">
+  
+  <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+  <meta http-equive="Content-Type" content="text/html; charset=UTF-8">
+
+  <meta name="theme-color" class="theme-color" content="#007acc">
+  <meta name="apple-mobile-web-app-status-bar-style" class="theme-color" content="#007acc">
+  <meta name="msapplication-navbutton-color" class="theme-color" content="#007acc">
+  <meta name="color-scheme" content="dark, light">
+  <meta name="msapplication-TileColor" content="#007acc">
+
+  <meta name="description" content="Notes Manger is a free webapplication notes thats create read, write, delete and protect multiple notes with securely. Register or signup in Notes Manger and get more notes security. Notes Manger will be online save, share, recieve and send multiple notes with securely. Notes manager develope by (Expert Modassir) on 14-10-2023 at 12:36 (UTC+12:40).">
+  <meta name="keywords" content="notes manager, sticky notes, notes, notes book, notes editor, simple notes, text notes, advance notes, notes backup, our notes, notes lite, popular notes, AI notes, web notes, App notes, digital notes, multi notes, notes sender, notes post, send recieve notes, data backups, personal notes, primary notes, user notes">
+  <meta itemprop="image" content="<?=$_SERVER["DOMAIN"];?>/snapshotnotes.png">
+
+  <meta property="og:title" content="Notes Manager">
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="<?=$_SERVER["DOMAIN"];?>">
+  <meta property="og:image" content="<?=$_SERVER["DOMAIN"];?>/snapshotnotes.png">
+  <meta property="og:site_name" content="Notes Manager">
+  <meta property="og:description" content="Notes Manger is a free webapplication notes thats create read, write, delete and protect multiple notes with securely. Register or signup in Notes Manger and get more notes security. Notes Manger will be online save, share, recieve and send multiple notes with securely. Notes manager develope by (Expert Modassir) on 14-10-2023 at 12:36 (UTC+12:40).">
+  <meta property="og:keywords" content="notes manager, sticky notes, notes, notes book, notes editor, simple notes, text notes, advance notes, notes backup, our notes, notes lite, popular notes, AI notes, web notes, App notes, digital notes, multi notes, notes sender, notes post, send recieve notes, data backups, personal notes, primary notes, user notes">
+
+  <meta name="twitter:card" content="summary">
+  <meta name="twitter:site" content="@notesmanager">
+  <meta name="twitter:title" content="Notes Manager">
+  <meta name="twitter:description" content="Notes Manger is a free webapplication notes thats create read, write, delete and protect multiple notes with securely. Register or signup in Notes Manger and get more notes security. Notes Manger will be online save, share, recieve and send multiple notes with securely. Notes manager develope by (Expert Modassir) on 14-10-2023 at 12:36 (UTC+12:40).">
+  <meta name="twitter:keywords" content="notes manager, sticky notes, notes, notes book, notes editor, simple notes, text notes, advance notes, notes backup, our notes, notes lite, popular notes, AI notes, web notes, App notes, digital notes, multi notes, notes sender, notes post, send recieve notes, data backups, personal notes, primary notes, user notes">
+  <meta name="twitter:creator" content="@notesmanager">
+  <meta name="twitter:image" content="<?=$_SERVER["DOMAIN"];?>/snapshotnotes.png">
+
+  <link rel="shortcut icon" sizes="32x32" href="images/icons/notes_favicon.ico" type="image/x-icon">
+  <link rel="apple-touch-icon" sizes="180x180" type="image/png" href="/images/icons/notes-touch-icon.png">
+  <link rel="icon" sizes="512x512" href="images/icons/notes-svgicon.svg" type="image/svg+xml">
+
+  <link rel="manifest" href="manifest.json">
+  <link rel="canonical" href="<?=$_SERVER["DOMAIN"];?>">
+
+  <link rel="stylesheet" type="text/css" href="themes/--var-themes.css">
+  <link rel="stylesheet" type="text/css" href="assets/static/app.css">
+  <!-- <link rel="stylesheet" href="/assets/static/prompt.css"> -->
+
+  <script type="text/javascript" src="libraries/jqrony-min.js?version=1.0.0"></script>
+  <script type="text/javascript" src="assets/modules/domrender.ui.js"></script>
+  <script type="text/javascript" src="assets/modules/app.ui.js" defer></script>
+  <script type="text/javascript" src="assets/snippets/snippets.html.js?cli=html"></script>
+  <script type="text/javascript" src="assets/snippets/snippets.js?cli=snippets"></script>
+  <script type="text/javascript" src="assets/snippets/snippets.random.js?cli=ra"></script>
+  <script type="text/javascript" src="assets/modules/ui-template-model.js"></script>
+  <script type="text/javascript" src="assets/modules/uiComponents.js" defer></script>
+  <script type="text/javascript" src="assets/javascript/controllers.main.js" defer></script>
+</head>
+<body data-theme="light_78b2914d270f42683c646a152310affd976831d9">
+  <!-- <div role="window" class="preloader Gok2N2nEj BO1Zf4qbZ PMiBu3q7U CGA3xkcxA fnoDqvqOH Xd6G1wnM1 DyBwOmsE" aria-hidden="true"><div class="grid-box CGA3xkcxA"><div class="cell" style="--delay: 0s"></div><div class="cell" style="--delay: 0.25s"></div><div class="cell" style="--delay: 0.5s"></div><div class="cell" style="--delay: 0.25s"></div><div class="cell" style="--delay: 0.5s"></div><div class="cell" style="--delay: 0.75s"></div><div class="cell" style="--delay: 0.5s"></div><div class="cell" style="--delay: 0.75s"></div><div class="cell" style="--delay: 1s"></div></div><div class="startup-logo"><div class="startup-cell"></div><div class="startup-cell"></div><div class="startup-cell"></div><div class="startup-cell"></div></div></div> -->
+  <noscript title="No Javascript Support">
+    <h2>You need to enable/update JavaScript to run this app.</h2>
+  </noscript>
+  <div id="content" class="BO1Zf4qbZ A6tyEeQ0U CGA3xkcxA"></div>
+</body>
+</html>
